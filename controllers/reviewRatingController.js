33ba96faafd1dbea242a03a0def8c074fcd63936
javid shabin <javid.main.act@gmail.com>
2024-05-17 15:30:13 +0530

    Create review rating controller and add to in the controller need finction for review and rating and export

---
+++ controllers/reviewRatingController.js
@@ -0,0 +1,27 @@
+// Create review
+const createReview = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Get review
+const getReview = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Update review
+const updateReview = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Remove review
+const removeReview = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  createReview,
+  getReview,
+  updateReview,
+  removeReview,
+};
