727977a346e0b4abdd12bcbe6354f72c6f72b8b6
javid shabin <javid.main.act@gmail.com>
2024-05-19 15:33:43 +0530

    Craete review rating router and export and call in v1 router

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,11 +3,13 @@ const { userRouter } = require('./userRouter')
 const { restaurantRouter } = require('./restautantRouter')
 const { menuRouter } = require('./menuRouter')
 const { cartRouter } = require('./cartRouter')
+const { reviewRatingRouter } = require('./reviewRatingRouter')
 const router = express.Router()
 
 router.use('/user', userRouter)
 router.use('/restaurant', restaurantRouter)
 router.use("/menu", menuRouter)
 router.use("/cart", cartRouter)
+router.use("/reivew", reviewRatingRouter)
 
 module.exports = {v1Router: router}
