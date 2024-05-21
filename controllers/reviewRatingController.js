3f9c5172343a047a12df2fdfc1e6bd503a8bd650
javid shabin <javid.main.act@gmail.com>
2024-05-21 17:40:09 +0530

    Creat add review fuction for review controller and rest rout using postman

--- controllers/reviewRatingController.js
+++ controllers/reviewRatingController.js
@@ -1,7 +1,50 @@
+const { Menu } = require("../models/menuModel");
+const { Restaurant } = require("../models/restaurant");
+const { Review } = require("../models/reviewRatingModel");
+
 // Create review
 const createReview = async (req, res) => {
   try {
-  } catch (error) {}
+    const userId = req.user.id;
+    // Destructure values from req.body
+    const { rating, comment, restaurant, menuItem } = req.body;
+    if (!rating || (restaurant === undefined && menuItem === undefined)) {
+      return res.status(400).json({
+        message:
+          "rating is required, and either restaurant or menuItem must be provided.",
+      });
+    }
+    // Check if restaurant
+    if (restaurant) {
+      const restaurantExists = await Restaurant.findById(restaurant);
+      if (!restaurantExists) {
+        return res.status(404).json({ message: "restaurant not found" });
+      }
+    }
+    // Check if menu menuItem exists
+    if (menuItem) {
+      const menuItemExists = await Menu.findById(menuItem);
+      if (!menuItemExists) {
+        return res.status(404).json({ message: "menu item not found" });
+      }
+    }
+    // Create a new review
+    const review = new Review({
+      user: userId,
+      restaurant,
+      menuItem,
+      rating,
+      comment,
+    });
+    // Save review to database
+    await review.save();
+    res.status(201).json({message: "Review added successfully", review});
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred while creating the review.",
+      error: error.message,
+    });
+  }
 };
 // Get review
 const getReview = async (req, res) => {
