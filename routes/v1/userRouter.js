f986abbc45f415038885c88badc6c7ea27650ecc
javid shabin <javid.main.act@gmail.com>
2024-05-24 15:48:46 +0530

    Create cloudinary and config

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -6,8 +6,10 @@ const {
   getAllUsers,
   userLogOut,
   userProfile,
+  updateUserProfile,
 } = require("../../controllers/userController");
 const { userAuth } = require("../../middleware/userAuth");
+const { upload } = require("../../middleware/multer");
 const router = express.Router();
 
 router.use("/register", userRegistration);
@@ -16,5 +18,6 @@ router.use("/login", userLogin);
 router.use("/users-list", getAllUsers);
 router.use("/logout", userLogOut);
 router.use("/user-profile", userAuth, userProfile);
+router.use("update-profile", userAuth, upload.single("image"), updateUserProfile)
 
 module.exports = { userRouter: router };
