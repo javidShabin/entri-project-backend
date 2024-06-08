44a2eb976d5f85aca5c2956021d48c5b300cfbf7
javid shabin <javid.main.act@gmail.com>
2024-06-08 21:18:18 +0530

    Create user model, controller file and datase connection

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+// Register user 
+const userRegistration = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+// Login user 
+// Logout user
+// Get user profile
+// Update user profile
+// Delete user
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors');
 const { apiRouter } = require('./routes');
+const { dbConnection } = require('./config/dbConnection');
 const port = 4000;
 
 app.use(cors())
 app.use(express.json())
+
+// Database connection
+dbConnection()
 
 app.use('api', apiRouter)
 
