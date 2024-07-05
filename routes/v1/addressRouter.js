025fa02c8aa18e99478629fd2b0f1482b3ea336a
javid shabin <javid.main.act@gmail.com>
2024-07-05 23:52:45 +0530

    Create routes bodys and endpoints for address

--- routes/v1/addressRouter.js
+++ routes/v1/addressRouter.js
@@ -2,6 +2,13 @@ const express = require("express")
 const { userAuth } = require("../../middleware/userAuth")
 const router = express.Router()
 
-
+// Create address
+router.post("/create-address")
+// Update address
+router.put("/update-adress")
+// Get address
+router.get("/get-address")
+// Delete address
+router.delete("/remove-address")
 
 module.exports = {addressRouter : router}
