dfc1eb121b14be74d84bcc142eea7497e934111a
javid shabin <javid.main.act@gmail.com>
2024-07-08 23:58:49 +0530

    Create the address component and create needed functional bodys for address and export then import to address router

---
+++ controllers/addressController.js
@@ -0,0 +1,24 @@
+
+const createAddress = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const updateAddress = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const getAddress = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const removeAddress = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+    createAddress,
+    updateAddress,
+    getAddress,
+    removeAddress
+}
--- routes/v1/addressRouter.js
+++ routes/v1/addressRouter.js
@@ -1,14 +1,20 @@
-const express = require("express")
-const { userAuth } = require("../../middleware/userAuth")
-const router = express.Router()
+const express = require("express");
+const { userAuth } = require("../../middleware/userAuth");
+const {
+  createAddress,
+  updateAddress,
+  getAddress,
+  removeAddress,
+} = require("../../controllers/addressController");
+const router = express.Router();
 
 // Create address
-router.post("/create-address")
+router.post("/create-address", userAuth, createAddress);
 // Update address
-router.put("/update-adress")
+router.put("/update-adress", updateAddress);
 // Get address
-router.get("/get-address")
+router.get("/get-address", userAuth, getAddress);
 // Delete address
-router.delete("/remove-address")
+router.delete("/remove-address", userAuth, removeAddress);
 
-module.exports = {addressRouter : router}
+module.exports = { addressRouter: router };
