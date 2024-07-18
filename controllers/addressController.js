f6b7a54c1545e8325a9158adb82a9a071aa7b3f0
javid shabin <javid.main.act@gmail.com>
2024-07-19 00:07:07 +0530

    Create and test the create address controller function using postman

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,6 +1,30 @@
+const { Address } = require("../models/addressModel");
 
+// Create address
 const createAddress = async (req, res) => {
   try {
+    // Destructur the data from req.body
+    const { name, email, street, city, postalCode, country, phone } = req.body;
+    const userId = req.user.id; // Get user id from req.user
+    // Check if requird fields are presented
+    if (!name || !street || !city || !postalCode || !phone) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+    // Add and save the address to datebade
+    const address = new Address({
+      name,
+      email,
+      street,
+      city,
+      postalCode,
+      phone,
+      country,
+      user: userId,
+    });
+    await address.save();
+    res.status(201).json(address); // Send the response including address
   } catch (error) {}
 };
 const updateAddress = async (req, res) => {
@@ -17,8 +41,8 @@ const removeAddress = async (req, res) => {
 };
 
 module.exports = {
-    createAddress,
-    updateAddress,
-    getAddress,
-    removeAddress
-}
+  createAddress,
+  updateAddress,
+  getAddress,
+  removeAddress,
+};
