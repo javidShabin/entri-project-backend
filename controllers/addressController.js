fa4e20fb52bf68a16e9269808edd481bcf088eb7
javid shabin <javid.main.act@gmail.com>
2024-07-29 00:13:51 +0530

    Create the get adderss function testing also

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -25,15 +25,34 @@ const createAddress = async (req, res) => {
     });
     await address.save();
     res.status(201).json(address); // Send the response including address
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 const updateAddress = async (req, res) => {
   try {
-  } catch (error) {}
+    const addressId = req.params.id; //Address id from req.params
+    const updatedData = req.body; // Updated address form req.body
+
+    // Find and add the updated address using id
+    const address = await Address.findByIdAndUpdate(addressId, updatedData, {
+      new: true,
+    });
+    res.status(200).json(address); // Send the resonse the updated uddress
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 const getAddress = async (req, res) => {
   try {
-  } catch (error) {}
+    const userId = req.user.id; // Get user id from reql.user
+    // Get the address using address id
+    const addresses = await Address.find({ user: userId });
+    // Send the response includig address
+    res.status(200).json(addresses);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 const removeAddress = async (req, res) => {
   try {
