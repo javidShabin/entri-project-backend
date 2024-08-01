eca42e71c34f24f3d5d57698ceff4dc801dd83a4
javid shabin <javid.main.act@gmail.com>
2024-08-02 00:36:24 +0530

    Make a little change

--- routes/v1/chatRouter.js
+++ routes/v1/chatRouter.js
@@ -2,13 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 // Send message
-router.post("/send", );
+router.post("/send",);
 // Chat history specific user
-router.get("/history/:userId", );
+router.get("/history/:userId",);
 // All user chat list
 router.get("/users", );
 // Clear all chat a specific user
-router.delete("/remove/:userId", );
+router.delete("/remove/:userId",);
 
 
 module.exports = {chatRouter : router}
