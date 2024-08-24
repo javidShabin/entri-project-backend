b40cb5eb50397445c6bc1c0fbb58c995e099c211
javid shabin <javid.main.act@gmail.com>
2024-08-24 12:03:08 +0530

    Create user login

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { userRegistration, verifyOtpAndCreateUser } = require("../../controllers/userController");
+const { userRegistration, verifyOtpAndCreateUser, userLogin } = require("../../controllers/userController");
 const router = express.Router();
 
 router.use("/register", userRegistration)
 router.use("/otpVerify", verifyOtpAndCreateUser)
+router.use("/login", userLogin)
 
 module.exports = { userRouter: router };
