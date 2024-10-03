4fa6105ba0dc8f3ab3e71c9370ea3fa7df8ecc91
javid shabin <javid.main.act@gmail.com>
2024-10-04 00:43:16 +0530

    Create needed functional bodys for chatcontroller

---
+++ controllers/chatController.js
@@ -0,0 +1,20 @@
+// Send message
+const sendMessage = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Get chat history
+const getChatHistory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Fetch a list of all users who have chatted with the admin
+const getUsersWithChats = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Clear all chats for a specific user
+const removeAllChats = async (req, res) => {
+  try {
+  } catch (error) {}
+};
