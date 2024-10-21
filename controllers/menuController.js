4e85a7729f8d5f1e3d80d44c4c8a5b0dc1c55542
javid shabin <javid.main.act@gmail.com>
2024-10-21 13:39:52 +0530

    Create menu controller and add need controoleres boy the it call in menu router for nd point

---
+++ controllers/menuController.js
@@ -0,0 +1,45 @@
+// Craate menu under a restaurant
+const createMenuItem = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Get the menu for a restaurant
+const getMenuforRestaurant = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Get menu by category
+const getMenusByCategory = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Search menu By name
+const searchMenuByName = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Filter menu by price
+const filterMenusByPrice = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Update a menu item
+const updateMenu = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Delete a menu item
+const deleteMenu = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  createMenuItem,
+  getMenuforRestaurant,
+  getMenusByCategory,
+  searchMenuByName,
+  filterMenusByPrice,
+  updateMenu,
+  deleteMenu,
+};
--- routes/v1/menuRouter.js
+++ routes/v1/menuRouter.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const {
+  createMenuItem,
+  getMenuforRestaurant,
+  getMenusByCategory,
+  searchMenuByName,
+  filterMenusByPrice,
+  updateMenu,
+  deleteMenu,
+} = require("../../controllers/menuController");
 const router = express.Router();
 
 // Create menu
-router.post("/create-menu",)
+router.post("/create-menu", createMenuItem);
 // Get menus by restaurant id
-router.get("/menu/:restaurantId",)
+router.get("/menu/:restaurantId", getMenuforRestaurant);
+// Get menu by gategory
+router.get("/menu/:restaurantId/:category", getMenusByCategory);
+// Search menu by name
+router.get("/menu/:restaurantId/search", searchMenuByName);
+// Filter menu bt price
+router.get("/menu/:restaurantId/filter/price", filterMenusByPrice);
 // Update menu
-router.put("/update-menu",)
+router.put("/update-menu", updateMenu);
 // delete menu
-router.delete("/remove-menu",)
-
+router.delete("/remove-menu", deleteMenu);
 
 module.exports = { menuRouter: router };
