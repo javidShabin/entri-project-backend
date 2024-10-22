af21174b7738e97aaf8fc0e9d861379f6c9e3d6e
javid shabin <javid.main.act@gmail.com>
2024-10-23 00:10:48 +0530

    Add routes to the cart router

--- routes/v1/cartRouter.js
+++ routes/v1/cartRouter.js
@@ -1,5 +1,18 @@
 const express = require("express")
+const { userAuth } = require("../../middleware/userAuth")
+const { addToCart, updateCart, cartDetails, removeFromCart, clearCart } = require("../../controllers/cartController")
 const router = express.Router()
+
+// Create cart
+router.post("/create-cart", userAuth, addToCart)
+// Update the cart
+router.put("/updateCart", userAuth, updateCart)
+// Get cart details
+router.get("/cart-details", userAuth, cartDetails)
+// Remove item from cart
+router.delete("/removeCart", userAuth, removeFromCart)
+// Clear the all  cart
+router.delete("/clear-cart", userAuth, clearCart)
 
 
 module.exports = {cartRouter : router}
