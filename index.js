9e550c8671d8982f663a9b5944e8f38292798766
javid shabin <javid.main.act@gmail.com>
2024-10-22 17:50:27 +0530

    Create api v1 user routes abd connect

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
+const { apiRouter } = require('./routes');
 const port = 4000;
 
 app.use(cors())
 app.use(express.json())
+
+app.use('api', apiRouter)
 
 app.listen(port, () => {
     console.log(`The server running in port${port}`)
