a321e670b5eeae334b1f325062d736055b8a59d8
javid shabin <javid.main.act@gmail.com>
2024-10-25 02:52:57 +0530

    Create restaurant update funcion in restaurant controller

--- routes/v1/restautantRouter.js
+++ routes/v1/restautantRouter.js
@@ -3,6 +3,7 @@ const {
   getAllRestaurants,
   getRestautantById,
   createRestaurant,
+  updateRestautant,
 } = require("../../controllers/restautantController");
 const { upload } = require("../../middleware/multer");
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/all-restaurants", getAllRestaurants);
 router.get("/:id", getRestautantById);
 router.post("/create-restaurant", upload.single("image"), createRestaurant);
+router.put("/:id", updateRestautant)
 
 module.exports = { restaurantRouter: router };
