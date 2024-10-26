c6a076d916b9cb3dd5685bb65d1b1e1db57b4d3c
javid shabin <javid.main.act@gmail.com>
2024-10-27 00:04:58 +0530

    Create cart controller and router, and creatin needed router body only in cart controller

---
+++ controllers/cartController.js
@@ -0,0 +1,32 @@
+// Add to cart
+const addToCart = async () => {
+  try {
+  } catch (error) {}
+};
+// Update the cart
+const updateCart = async () => {
+  try {
+  } catch (error) {}
+};
+// Cart detials
+const cartDetails = async () => {
+  try {
+  } catch (error) {}
+};
+// Remove from cart
+const removeFromCart = async () => {
+  try {
+  } catch (error) {}
+};
+// Clear cart
+const clearCart = async () => {
+  try {
+  } catch (error) {}
+};
+module.exports = {
+    addToCart,
+    updateCart,
+    cartDetails,
+    removeFromCart,
+    clearCart
+}
