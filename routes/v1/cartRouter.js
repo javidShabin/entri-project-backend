3d533db67fddfa57b24590e2dd22c0cbcb3bfee1
javid shabin <javid.main.act@gmail.com>
2024-11-01 01:40:04 +0530

    Make little change

--- routes/v1/cartRouter.js
+++ routes/v1/cartRouter.js
@@ -1,18 +1,23 @@
-const express = require("express")
-const {  addItemToCart, updateCart, getCartDetails, removeFromCart, clearCart, } = require("../../controllers/cartController")
-const { userAuth } = require("../../middleware/userAuth")
-const router = express.Router()
+const express = require("express");
+const {
+  addItemToCart,
+  updateCart,
+  getCartDetails,
+  removeFromCart,
+  clearCart,
+} = require("../../controllers/cartController");
+const { userAuth } = require("../../middleware/userAuth");
+const router = express.Router();
 
 // Create cart
-router.post("/create-cart", userAuth, addItemToCart)
+router.post("/create-cart", userAuth, addItemToCart);
 // Update the cart
-router.put("/updateCart", userAuth, updateCart)
+router.put("/updateCart", userAuth, updateCart);
 // Get cart details
-router.get("/cart-details", userAuth, getCartDetails)
+router.get("/cart-details", userAuth, getCartDetails);
 // Remove item from cart
-router.delete("/removeCart", userAuth, removeFromCart)
+router.delete("/removeCart", userAuth, removeFromCart);
 // Clear the all  cart
-router.delete("/clear-cart", userAuth, clearCart)
+router.delete("/clear-cart", userAuth, clearCart);
 
-
-module.exports = {cartRouter : router}
+module.exports = { cartRouter: router };
